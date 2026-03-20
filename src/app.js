da4dabc5d06c635a5f372bddeb74a8ec6cece403
Latika Gurudu <lmngurudu@gmail.com>
2026-03-20 23:24:07 +0530

    deployment_checking

--- src/app.js
+++ src/app.js
@@ -24,6 +24,10 @@ const authRouter = require('./routes/auth.routes');
 const accountRouter = require('./routes/account.routes');
 const transactionRoutes = require('./routes/transaction.routes');
 
+app.use("/", (req, res) => {
+    console.log("Server is running");
+    res.send("Server is running");
+})
 // Use Routes
 app.use('/api/auth', authRouter);
 app.use('/api/accounts', accountRouter);
